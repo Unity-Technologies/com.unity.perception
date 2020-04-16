da542dd4b0e68e9ec50160fa5b66211ca20f7d63
Jon Hogins <jonathanh@unity3d.com>
2020-04-16 11:04:29 -0700

    Fixing LabelingConfigurationEditor refresh and save bugs

--- com.unity.perception/Editor/GroundTruth/LabelingConfigurationEditor.cs
+++ com.unity.perception/Editor/GroundTruth/LabelingConfigurationEditor.cs
@@ -20,37 +20,51 @@ public class LabelingConfigurationEditor : Editor
     void OnRemove(ReorderableList list)
     {
         if (list.index != -1)
-            config.LabelingConfigurations.RemoveAt(list.index);
+            list.serializedProperty.DeleteArrayElementAtIndex(list.index);
+
+        this.serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(target);
     }
 
     LabelingConfiguration config => (LabelingConfiguration)this.target;
 
     void OnAdd(ReorderableList list)
     {
-        config.LabelingConfigurations.Add(new LabelingConfigurationEntry("", 0));
+        var index = list.serializedProperty.arraySize;
+        list.serializedProperty.InsertArrayElementAtIndex(index);
+        var element = list.serializedProperty.GetArrayElementAtIndex(index);
+        var labelProperty = element.FindPropertyRelative(nameof(LabelingConfigurationEntry.label));
+        labelProperty.stringValue = "";
+        var valueProperty = element.FindPropertyRelative(nameof(LabelingConfigurationEntry.value));
+        valueProperty.intValue = 0;
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(target);
     }
 
     void DrawElement(Rect rect, int index, bool isactive, bool isfocused)
     {
-        var entry = config.LabelingConfigurations[index];
+        var element = m_LabelsList.serializedProperty.GetArrayElementAtIndex(index);
+        var labelProperty = element.FindPropertyRelative(nameof(LabelingConfigurationEntry.label));
+        var valueProperty = element.FindPropertyRelative(nameof(LabelingConfigurationEntry.value));
         using (var change = new EditorGUI.ChangeCheckScope())
         {
             var contentRect = new Rect(rect.position, new Vector2(rect.width, EditorGUIUtility.singleLineHeight));
-            var newLabel = EditorGUI.TextField(contentRect, nameof(LabelingConfigurationEntry.label), entry.label);
+            var newLabel = EditorGUI.TextField(contentRect, nameof(LabelingConfigurationEntry.label), labelProperty.stringValue);
             if (change.changed)
-                config.LabelingConfigurations[index] = new LabelingConfigurationEntry(newLabel, entry.value);
+                labelProperty.stringValue = newLabel;
         }
         using (var change = new EditorGUI.ChangeCheckScope())
         {
             var contentRect = new Rect(rect.position + new Vector2(0, EditorGUIUtility.singleLineHeight), new Vector2(rect.width, EditorGUIUtility.singleLineHeight));
-            var newValue = EditorGUI.IntField(contentRect, nameof(LabelingConfigurationEntry.value), entry.value);
+            var newValue = EditorGUI.IntField(contentRect, nameof(LabelingConfigurationEntry.value), valueProperty.intValue);
             if (change.changed)
-                config.LabelingConfigurations[index] = new LabelingConfigurationEntry(entry.label, newValue);
+                valueProperty.intValue = newValue;
         }
     }
 
     public override void OnInspectorGUI()
     {
         m_LabelsList.DoLayoutList();
+        this.serializedObject.ApplyModifiedProperties();
     }
 }

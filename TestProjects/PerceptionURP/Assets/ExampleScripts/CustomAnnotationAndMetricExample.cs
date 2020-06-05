e65d8000a46223c0add8206c675da42758d06b6c
Jon Hogins <jonathanh@unity3d.com>
2020-06-05 09:03:46 -0700

    Adding CustomAnnotationAndMetricExample and adding content to SimulationManager.md

---
+++ TestProjects/PerceptionURP/Assets/ExampleScripts/CustomAnnotationAndMetricExample.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
+
+[RequireComponent(typeof(PerceptionCamera))]
+public class CustomAnnotationAndMetricExample : MonoBehaviour
+{
+    public GameObject light;
+    public GameObject target;
+
+    MetricDefinition lightPositionMetricDefinition;
+    AnnotationDefinition targetBoundingBoxAnnotationDefinition;
+    SensorHandle cameraSensorHandle;
+
+    public void Start()
+    {
+        //Metrics and annotations are registered up-front and are referenced later when values are reported
+        lightPositionMetricDefinition = SimulationManager.RegisterMetricDefinition(
+            "Light position",
+            "The world-space position of the light",
+            Guid.Parse("1F6BFF46-F884-4CC5-A878-DB987278FE35"));
+        targetBoundingBoxAnnotationDefinition = SimulationManager.RegisterAnnotationDefinition(
+            "Target bounding box",
+            "The axis-aligned bounding box of the target in the camera's local space",
+            id: Guid.Parse("C0B4A22C-0420-4D9F-BAFC-954B8F7B35A7"));
+    }
+
+    public void Update()
+    {
+        
+        //Report the light's position by manually creating the json array string.
+        var lightPosition = light.transform.position;
+        SimulationManager.ReportMetric(lightPositionMetricDefinition,
+            $@"[{{ ""x"": {lightPosition.x}, ""y"": {lightPosition.y}, ""z"": {lightPosition.z} }}]");
+        //compute the location of the object in the camera's local space
+        var targetCameraLocalPosition = transform.worldToLocalMatrix * target.transform.position;
+        //Report the annotation on the camera SensorHandle exposed by the PerceptionCamera
+        GetComponent<PerceptionCamera>().SensorHandle.ReportAnnotationValues(targetBoundingBoxAnnotationDefinition,new[] { targetCameraLocalPosition });
+    }
+}
+
+//
+// {
+//   "id": "71265896-2a46-405a-a3d9-e587cdfac631",
+//   "annotation_definition": "c0b4a22c-0420-4d9f-bafc-954b8f7b35a7",
+//   "values": [
+//     {
+//       "Center": {
+//         "x": -85.386672973632813,
+//         "y": 84.000732421875,
+//         "z": 112.38008880615234
+//       },
+//       "Extents": {
+//         "x": 0.64206844568252563,
+//         "y": 0.71592754125595093,
+//         "z": 0.66213905811309814
+//       }
+//     }
+//   ]
+// },
--- com.unity.perception/Documentation~/SimulationManager.md
+++ com.unity.perception/Documentation~/SimulationManager.md
@@ -1,8 +1,11 @@
 # SimulationManager
 
-The SimulationManager tracks egos, sensors, annotations, and metrics, combining them into a unified [JSON-based dataset](Schema/Synthetic_Dataset_Schema.md) on disk. While sensors are registered, the SimulationManager ensures that frames are deterministic and run at the appropriate simulation times to let each sensor run at its own rate.
+`SimulationManager` tracks egos, sensors, annotations, and metrics, combining them into a unified [JSON-based dataset](Schema/Synthetic_Dataset_Schema.md) on disk. It also controls the simulation time elapsed per frame to accommodate the active sensors.
 
 ## Sensor scheduling
+ While sensors are registered, `SimulationManager` ensures that frame timing is deterministic and run at the appropriate simulation times to let each sensor run at its own rate.
+ 
+ Using [Time.CaptureDeltaTime](https://docs.unity3d.com/ScriptReference/Time-captureDeltaTime.html), it also decouples wall clock time from simulation time, allowing the simulation to run as fast as possible.
 
 ## Custom annotations and metrics
 

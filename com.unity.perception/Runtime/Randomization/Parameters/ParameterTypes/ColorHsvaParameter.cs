ee0003d8da4fbaa14d479745213ea592d7cac8e5
Steven Leal <steven.leal@unity3d.com>
2020-07-18 21:14:26 -0500

    added alpha component to color parameter

--- com.unity.perception/Runtime/Randomization/Parameters/ParameterTypes/ColorHsvaParameter.cs
+++ com.unity.perception/Runtime/Randomization/Parameters/ParameterTypes/ColorHsvaParameter.cs
@@ -5,21 +5,24 @@ using UnityEngine.Perception.Randomization.Samplers;
 namespace UnityEngine.Perception.Randomization.Parameters
 {
     [AddComponentMenu("")]
-    [ParameterMetaData("ColorHSV")]
-    public class ColorHsvParameter : TypedParameter<Color>
+    [ParameterMetaData("ColorHSVA")]
+    public class ColorHsvaParameter : TypedParameter<Color>
     {
         public Sampler hue;
         public Sampler saturation;
         public Sampler value;
+        public Sampler alpha;
 
-        public override Sampler[] Samplers => new []{ hue, saturation, value };
+        public override Sampler[] Samplers => new []{ hue, saturation, value, alpha };
 
         public override Color Sample(int iteration)
         {
-            return Color.HSVToRGB(
+            var color = Color.HSVToRGB(
                 hue.Sample(iteration),
                 saturation.Sample(iteration),
                 value.Sample(iteration));
+            color.a = alpha.Sample(iteration);
+            return color;
         }
 
         public override Color[] Samples(int iteration, int sampleCount)
@@ -28,12 +31,15 @@ namespace UnityEngine.Perception.Randomization.Parameters
             var hueRng = hue.GetRandom(iteration);
             var satRng = saturation.GetRandom(iteration);
             var valRng = value.GetRandom(iteration);
+            var alphaRng = value.GetRandom(iteration);
             for (var i = 0; i < sampleCount; i++)
             {
-                samples[i] = Color.HSVToRGB(
+                var color = Color.HSVToRGB(
                     hue.Sample(ref hueRng),
                     saturation.Sample(ref satRng),
                     value.Sample(ref valRng));
+                color.a = alpha.Sample(ref alphaRng);
+                samples[i] = color;
             }
             return samples;
         }

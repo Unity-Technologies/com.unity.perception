c93add55f8a5ce9d164d10e6067c83cf00b05a87
Jon Hogins <jonathanh@unity3d.com>
2020-07-31 10:35:59 -0700

    Removing final usage of UnityEditorInternal to fix tests

--- com.unity.perception/Tests/Runtime/GroundTruthTests/SegmentationGroundTruthTests.cs
+++ com.unity.perception/Tests/Runtime/GroundTruthTests/SegmentationGroundTruthTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Unity.Collections;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Perception.GroundTruth;
 using UnityEngine.Rendering;

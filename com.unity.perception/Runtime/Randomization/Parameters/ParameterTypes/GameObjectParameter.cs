886c933fb9195b4b6f69e0a7144a5cf705758a90
Steven Leal <steven.leal@unity3d.com>
2020-08-14 17:25:01 -0500

    added GameObject categorical parameter

---
+++ com.unity.perception/Runtime/Randomization/Parameters/ParameterTypes/GameObjectParameter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine.Perception.Randomization.Parameters.Attributes;
+
+namespace UnityEngine.Perception.Randomization.Parameters
+{
+    [Serializable]
+    [ParameterMetaData("GameObject")]
+    public class GameObjectParameter : CategoricalParameter<GameObject> { }
+}

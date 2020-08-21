25221bf122bc30c831cef8547fc0f84643d77147
Steve Borkman <66975693+StevenBorkman@users.noreply.github.com>
2020-08-20 23:11:25 -0400

    Aisv760 editor bugs - fixes for 759, 760, 761 (#58)
    * Multiple object editing
    
    * Remove indents from inspector
    
    * Fix to commit changes on a prefab labeling edit
    
    * Responsive label editing
    
    Fix so that add label does not lock up
    
    * Labeling editor fixes
    
    * Remove placeholder text
    
    Hopefully only temporary, but it seems to be almost impossible to get placeholder text to work in current gui implementation

--- com.unity.perception/Editor/GroundTruth/LabelingEditor.cs
+++ com.unity.perception/Editor/GroundTruth/LabelingEditor.cs
@@ -1,13 +1,13 @@
+using Unity.Entities;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Perception.GroundTruth;
 
 namespace UnityEditor.Perception.GroundTruth
 {
-    [CustomEditor(typeof(Labeling))]
+    [CustomEditor(typeof(Labeling)), CanEditMultipleObjects]
     class LabelingEditor : Editor
     {
-        const int k_Indent = 7;
         ReorderableList m_LabelsList;
 
         public void OnEnable()
@@ -16,34 +16,101 @@ namespace UnityEditor.Perception.GroundTruth
             m_LabelsList.drawElementCallback = DrawElement;
             m_LabelsList.onAddCallback += OnAdd;
             m_LabelsList.onRemoveCallback += OnRemove;
+            m_LabelsList.onReorderCallbackWithDetails += OnReordered;
         }
 
         void OnRemove(ReorderableList list)
         {
             if (list.index != -1)
-                labeling.labels.RemoveAt(list.index);
+            {
+                var value = labeling.labels[list.index];
+                foreach (var t in targets)
+                {
+                    ((Labeling)t).labels.Remove(value);
+                }
+            }
         }
 
         Labeling labeling => (Labeling)target;
 
         void OnAdd(ReorderableList list)
         {
-            labeling.labels.Add("");
+            foreach (var t in targets)
+            {
+                var castedTarget = ((Labeling)t);
+                castedTarget.labels.Add("");
+                EditorUtility.SetDirty(castedTarget);
+            }
+        }
+
+        void OnReordered(ReorderableList list, int oldIndex, int newIndex)
+        {
+            var label = labeling.labels[newIndex];
+
+            foreach (var t in targets)
+            {
+                var l = (Labeling)t;
+                if (this.labeling == l) continue;
+
+                ReorderLabels(l, label, newIndex);
+            }
+        }
+
+        static void ReorderLabels(Labeling labeling, string label, int newIndex)
+        {
+            if (labeling.labels.Contains(label))
+            {
+                labeling.labels.Remove(label);
+                if (newIndex < labeling.labels.Count)
+                    labeling.labels.Insert(newIndex, label);
+                else
+                    labeling.labels.Add(label);
+            }
+        }
+
+        static void ReplaceLabel(Labeling labeling, string oldLabel, string newLabel)
+        {
+            var idx = labeling.labels.IndexOf(oldLabel);
+            if (idx == -1) return;
+            labeling.labels[idx] = newLabel;
+        }
+
+        private void ReplaceLabel(int index, string newLabel)
+        {
+            labeling.labels[index] = newLabel;
+        }
+
+        void ReplaceLabelAll(int index, string currentLabel)
+        {
+            var oldLabel = labeling.labels[index];
+            ReplaceLabel(index, currentLabel);
+
+            foreach (var t in targets)
+            {
+                var l = (Labeling)t;
+
+                if (this.labeling == l) continue;
+
+                ReplaceLabel(l, oldLabel, currentLabel);
+            }
         }
 
         void DrawElement(Rect rect, int index, bool isactive, bool isfocused)
         {
             using (var change = new EditorGUI.ChangeCheckScope())
             {
-                var indent = k_Indent * index;
-                if (indent >= rect.width)
-                    return;
+                var contentRect = new Rect(rect.x, rect.y, rect.width, rect.height);
 
-                var contentRect = new Rect(rect.x + indent, rect.y, rect.width - indent, rect.height);
-                var value = EditorGUI.TextField(contentRect, labeling.labels[index]);
+                var value = EditorGUI.DelayedTextField(contentRect, labeling.labels[index]);
+
                 if (change.changed)
                 {
-                    labeling.labels[index] = value;
+                    ReplaceLabelAll(index, value);
+
+                    if (PrefabUtility.IsPartOfAnyPrefab(target))
+                    {
+                        EditorUtility.SetDirty(target);
+                    }
                 }
             }
         }

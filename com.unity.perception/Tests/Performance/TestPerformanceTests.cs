89acb9362279ce7e99027f0bb2764c7996f2a38d
Steven Borkman <steven.borkman@unity3d.com>
2020-09-28 10:36:36 -0400

    testing recurring jobs

--- com.unity.perception/Tests/Performance/TestPerformanceTests.cs
+++ com.unity.perception/Tests/Performance/TestPerformanceTests.cs
@@ -10,15 +10,15 @@ using UnityEngine.UIElements;
 
 namespace PerformanceTests
 {
-    [TestFixture(640, 480, false, false)]//, Category("Performance")]
-    [TestFixture(640, 480, true, false)]//, Category("Performance")]
-    [TestFixture(640, 480, true, true)]//, Category("Performance")]
-    [TestFixture(1024, 768, false, false)]//, Category("Performance")]
-    [TestFixture(1024, 768, true, false)]//, Category("Performance")]
-    [TestFixture(1024, 768, true, true)]//, Category("Performance")]
-    [TestFixture(1920, 1080, false, false)]//, Category("Performance")]
-    [TestFixture(1920, 1080, true, false)]//, Category("Performance")]
-    [TestFixture(1920, 1080, true, true)]//, Category("Performance")]
+    [TestFixture(640, 480, false, false)]
+    [TestFixture(640, 480, true, false)]
+    [TestFixture(640, 480, true, true)]
+    [TestFixture(1024, 768, false, false)]
+    [TestFixture(1024, 768, true, false)]
+    [TestFixture(1024, 768, true, true)]
+    [TestFixture(1920, 1080, false, false)]
+    [TestFixture(1920, 1080, true, false)]
+    [TestFixture(1920, 1080, true, true)]
     [Category("Performance")]
     public class TestPerformanceTestsTwo
     {

216215cdcb28eb527c92b050981b2b56eec3fc70
Steven Leal <steven.leal@unity3d.com>
2021-01-08 14:44:47 -0600

    added edit-mode test

---
+++ com.unity.perception/Tests/Editor/RandomizerEditorTests.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Experimental.Perception.Randomization.Randomizers;
+using UnityEngine.Experimental.Perception.Randomization.Scenarios;
+
+namespace EditorTests
+{
+    [TestFixture]
+    public class RandomizerEditorTests
+    {
+        GameObject m_TestObject;
+        FixedLengthScenario m_Scenario;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_TestObject = new GameObject();
+            m_Scenario = m_TestObject.AddComponent<FixedLengthScenario>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(m_TestObject);
+        }
+
+        [Test]
+        public void RandomizerOnCreateMethodNotCalledInEditMode()
+        {
+            // TestRandomizer.OnCreate() should NOT be called here while in edit-mode
+            // if ScenarioBase.CreateRandomizer<>() was coded correctly
+            Assert.DoesNotThrow(() =>
+            {
+                m_Scenario.CreateRandomizer<ErrorsOnCreateTestRandomizer>();
+            });
+        }
+    }
+
+    [AddRandomizerMenu("Test Randomizers/Errors OnCreate Test Randomizer")]
+    class ErrorsOnCreateTestRandomizer : Randomizer
+    {
+        public GameObject testGameObject;
+
+        protected override void OnCreate()
+        {
+            testGameObject.transform.position = Vector3.zero;
+        }
+    }
+}

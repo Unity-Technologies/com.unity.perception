cf7645b3e03d27804c67c7a072f72345c0c3e78c
Mohsen Kamalzadeh <>
2021-01-13 11:42:48 -0800

    made integration samples public

--- com.unity.perception/Runtime/Randomization/Samplers/SamplerTypes/AnimationCurveSampler.cs
+++ com.unity.perception/Runtime/Randomization/Samplers/SamplerTypes/AnimationCurveSampler.cs
@@ -26,12 +26,17 @@ namespace UnityEngine.Experimental.Perception.Randomization.Samplers
         [Tooltip("Probability distribution curve used for this sampler. The X axis corresponds to the values this sampler will pick from, and the Y axis corresponds to the relative probability of the values. The relative probabilities (Y axis) do not need to max out at 1 as only the shape of the curve matters. The Y values cannot however be negative.")]
         public AnimationCurve distributionCurve;
 
+        /// <summary>
+        /// Number of samples used for integrating over the provided AnimationCurve. The larger the number of samples, the more accurate the resulting probability distribution will be.
+        /// </summary>
+        [Tooltip("Number of internal samples used for integrating over the provided AnimationCurve. The larger the number of samples, the more accurately the resulting probability distribution will follow the provided AnimationCurve. Increase this if the default value proves insufficient.")]
+        public int numOfSamplesForIntegration = 1000;
+
         float[] m_IntegratedCurve;
         bool m_CurveValid;
         float m_StartTime;
         float m_EndTime;
         float m_Interval;
-        int m_NumOfSamples = 100;
 
         /// <summary>
         /// Constructs an Animation Curve Sampler
@@ -74,13 +79,13 @@ namespace UnityEngine.Experimental.Perception.Randomization.Samplers
 
             if (m_IntegratedCurve == null)
             {
-                m_IntegratedCurve = new float[m_NumOfSamples];
+                m_IntegratedCurve = new float[numOfSamplesForIntegration];
 
                 SamplerUtility.IntegrateCurve(m_IntegratedCurve, distributionCurve);
 
                 m_StartTime = distributionCurve.keys[0].time;
                 m_EndTime = distributionCurve.keys[distributionCurve.length - 1].time;
-                m_Interval = (m_EndTime - m_StartTime) / (m_NumOfSamples - 1);
+                m_Interval = (m_EndTime - m_StartTime) / (numOfSamplesForIntegration - 1);
             }
         }
     }

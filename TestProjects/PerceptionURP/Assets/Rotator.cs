907ed447697d2dad84609ad94936afc488203b1b
Mohsen Kamalzadeh <>
2021-01-25 16:39:50 -0800

    reverted unwanted commit

--- TestProjects/PerceptionURP/Assets/Rotator.cs
+++ TestProjects/PerceptionURP/Assets/Rotator.cs
@@ -1,34 +1,13 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Rendering;
+﻿using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
     [SerializeField]
     public float yDegreesPerSecond = 180;
-    DateTime prev = DateTime.Now;
-    double[] difs = new double[1000];
-    int count = 0;
+
     // Update is called once per frame
     void Update()
     {
-        var now = DateTime.Now;
-        var dif = now - prev;
         transform.localRotation *= Quaternion.Euler(0, yDegreesPerSecond * Time.deltaTime, 0);
-        prev = now;
-        if (count < difs.Length)
-        {
-            difs[count] = dif.TotalMilliseconds;
-        }
-        else
-        {
-            Debug.Log("DIFS==================");
-            for (int i = 0; i < difs.Length; i++)
-            {
-                Debug.Log(difs[i]+"\n");
-            }
-        }
-
-        count++;
     }
 }

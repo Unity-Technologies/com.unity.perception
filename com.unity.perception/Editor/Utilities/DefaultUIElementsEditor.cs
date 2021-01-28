1dbbbd180e0f0fa616200dbadb76801e3fd17c37
Steven Leal <steven.leal@unity3d.com>
2021-01-27 18:04:37 -0600

    added derivable default UI elements editor

---
+++ com.unity.perception/Editor/Utilities/DefaultUIElementsEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace UnityEngine.Experimental.Perception.Editor
+{
+    /// <summary>
+    /// Derive this class to force the Unity Editor to render an Object's default inspector using UIElements
+    /// </summary>
+    public abstract class DefaultUIElementsEditor : UnityEditor.Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            var rootElement = new VisualElement();
+            CreatePropertyFields(rootElement);
+            return rootElement;
+        }
+
+        void CreatePropertyFields(VisualElement rootElement)
+        {
+            var iterator = serializedObject.GetIterator();
+            iterator.NextVisible(true);
+            do
+            {
+                if (iterator.name == "m_Script")
+                    continue;
+                var propertyField = new PropertyField(iterator.Copy());
+                propertyField.Bind(serializedObject);
+                rootElement.Add(propertyField);
+            } while (iterator.NextVisible(false));
+        }
+    }
+}

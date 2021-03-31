150d0601c0d9c6e7464da7238f5a5e6b0cff2410
sleal-unity <steven.leal@unity3d.com>
2021-03-31 18:48:06 -0500

    added first frame skipping back to local editor runs

--- com.unity.perception/Runtime/Randomization/Scenarios/ScenarioBase.cs
+++ com.unity.perception/Runtime/Randomization/Scenarios/ScenarioBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Simulation;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Perception.Randomization.Parameters;

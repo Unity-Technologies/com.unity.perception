8a6ae20d4f4fbc7eeeca96791529495a5d3bf4d5
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-05-06 16:18:48 -0700

    added object bounds normalization option to sample foreground object placement randomizer

--- com.unity.perception/Runtime/Randomization/Randomizers/RandomizerExamples/Randomizers/ForegroundObjectPlacementRandomizer.cs
+++ com.unity.perception/Runtime/Randomization/Randomizers/RandomizerExamples/Randomizers/ForegroundObjectPlacementRandomizer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using SynthDet.Randomizers;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine.Perception.Randomization.Parameters;
 using UnityEngine.Perception.Randomization.Randomizers.Utilities;
 using UnityEngine.Perception.Randomization.Samplers;
@@ -40,6 +43,11 @@ namespace UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers
         GameObject m_Container;
         GameObjectOneWayCache m_GameObjectOneWayCache;
 
+        /// <summary>
+        /// Enables object bounds normalization across all included objects, so that they all have a similar size before any further scale adjustments.
+        /// </summary>
+        [Tooltip("Enable this to normalize mesh sizes across all included objects, so that they all have a similar size before any further scale adjustments are applied during randomization. Note that this flag greatly influences the size of the objects on the screen, so any scale randomization will need to be adjusted based on the state of this flag.")]
+        public bool normalizeObjectBounds;
         /// <inheritdoc/>
         protected override void OnAwake()
         {
@@ -61,7 +69,25 @@ namespace UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers
             foreach (var sample in placementSamples)
             {
                 var instance = m_GameObjectOneWayCache.GetOrInstantiate(prefabs.Sample());
-                instance.transform.position = new Vector3(sample.x, sample.y, depth) + offset;
+
+                if (normalizeObjectBounds)
+                {
+                    instance.transform.localPosition = Vector3.zero;
+                    instance.transform.localScale = Vector3.one;
+                    instance.transform.localRotation = Quaternion.identity;
+                    var bounds = ComputeBounds(instance);
+
+                    instance.transform.localPosition = new Vector3(sample.x, sample.y, depth) + offset - bounds.center;
+                    var scale = instance.transform.localScale;
+                    var magnitude = bounds.extents.magnitude;
+                    scale.Scale(new Vector3(1/magnitude, 1/magnitude, 1/magnitude));
+                    instance.transform.localScale = scale;
+                }
+                else
+                {
+                    instance.transform.position = new Vector3(sample.x, sample.y, depth) + offset;
+                }
+
             }
             placementSamples.Dispose();
         }
@@ -72,6 +98,54 @@ namespace UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers
         protected override void OnIterationEnd()
         {
             m_GameObjectOneWayCache.ResetAllObjects();
+        }
+
+        static NativeArray<Bounds> ComputeObjectBounds(GameObject[] prefabs)
+        {
+            var objectBounds = new NativeArray<Bounds>(prefabs.Length, Allocator.TempJob);
+            for (int i = 0; i < prefabs.Length; i++)
+            {
+                var bounds = ComputeBounds(prefabs[i]);
+                //assume objects will be aligned at origin
+                bounds.center = Vector3.zero;
+                objectBounds[i] = bounds;
+            }
+
+            return objectBounds;
+        }
+
+        static Bounds ComputeBounds(GameObject gameObject)
+        {
+            var bounds = ComputeBoundsUnchecked(gameObject);
+            if (!bounds.IsValid)
+                throw new ArgumentException($"GameObject {gameObject.name} must have a MeshFilter in its hierarchy.");
+
+            var result = new Bounds();
+            result.SetMinMax(bounds.Min, bounds.Max);
+            return result;
+        }
+
+        static SynthDetMinMaxAABB ComputeBoundsUnchecked(GameObject gameObject)
+        {
+            SynthDetMinMaxAABB aabb = new SynthDetMinMaxAABB(
+                new float3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity),
+                new float3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity));
+            var meshFilter = gameObject.GetComponent<MeshFilter>();
+            if (meshFilter != null)
+            {
+                var bounds = meshFilter.sharedMesh.bounds;
+                aabb = SynthDetMinMaxAABB.CreateFromCenterAndExtents(bounds.center, bounds.extents);
+            }
+
+            var transform = gameObject.transform;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var childAabb = ComputeBoundsUnchecked(transform.GetChild(i).gameObject);
+                aabb.Encapsulate(childAabb);
+            }
+
+            aabb = SynthDetMinMaxAABB.Transform(float4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale), aabb);
+            return aabb;
         }
     }
 }

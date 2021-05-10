6ab2e0b6c2bd592e1c19b0fb3ec9ec2a0c88900e
leopoldo-zugasti <leopoldo.zugasti@unity3d.com>
2021-05-10 15:04:37 -0400

    hide command prompt and use a progress bar instead

--- com.unity.perception/Editor/Pyrception/PyrceptionInstaller.cs
+++ com.unity.perception/Editor/Pyrception/PyrceptionInstaller.cs
@@ -1,14 +1,17 @@
 using System.Diagnostics;
+using System.Threading;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Perception.GroundTruth;
 
-public class PyrceptionInstaller
-{
+public class PyrceptionInstaller : EditorWindow
+{  
+
     [MenuItem("Window/General/Pyrception/Run")]
     static void RunPyrception()
     {
+
         string path = Application.dataPath.Replace("/Assets","").Replace("/","\\");
         string command = $"cd {path}\\DataInsightsEnv\\Scripts\\ && activate && pyrception-utils.exe preview --data=\"{PlayerPrefs.GetString(SimulationState.latestOutputDirectoryKey)}/..\"";
 
@@ -21,24 +24,34 @@ public class PyrceptionInstaller
     [MenuItem("Window/General/Pyrception/Setup")]
     static void SetupPyrception()
     {
+        int steps = 4;
+
         string path = Application.dataPath.Replace("/Assets", "").Replace("/", "\\");
         string pyrceptionPath = Path.GetFullPath("Packages/com.unity.perception/Editor/Pyrception/pyrception-utils");
 
+        EditorUtility.DisplayProgressBar("Setting up Pyrception", "Installing virtualenv...", 0 / steps);
         int ExitCode = 0;
         ExitCode = ExecuteCMD("pip install virtualenv");
         if (ExitCode != 0) return;
 
+        EditorUtility.DisplayProgressBar("Setting up Pyrception", "Setting up virtualenv instance...", 1f / steps);
         ExitCode = ExecuteCMD($"virtualenv \"{path}\\DataInsightsEnv\"");
         if (ExitCode != 0) return;
 
+        EditorUtility.DisplayProgressBar("Setting up Pyrception", "Getting pyrception files...", 2f / steps);
         ExitCode = ExecuteCMD($"XCOPY /E/I/Y \"{pyrceptionPath}\" \"{path}\\DataInsightsEnv\\pyrception-util\"");
         if (ExitCode != 0) return;
 
+        EditorUtility.DisplayProgressBar("Setting up Pyrception", "Installing pyrception utils...", 3.5f / steps);
         ExitCode = ExecuteCMD($"cd \"{path}\\DataInsightsEnv\\pyrception-util\"");
         if (ExitCode != 0) return;
 
+        EditorUtility.DisplayProgressBar("Setting up Pyrception", "Installing pyrception utils...", 3.9f / steps);
         ExitCode = ExecuteCMD($"\"{path}\\DataInsightsEnv\\Scripts\\activate\" && cd {path} && cd .\\DataInsightsEnv\\pyrception-util\\ && pip --no-cache-dir install -e . && deactivate");
         if (ExitCode != 0) return;
+
+
+        EditorUtility.ClearProgressBar();
     }
 
     private static int ExecuteCMD(string command) {
@@ -55,8 +68,13 @@ public class PyrceptionInstaller
         {
             UnityEngine.Debug.LogError($"Error - {ExitCode} - Failed to execute: {command}");
         }
+        else
+        {
+            //UnityEngine.Debug.Log($"Successfully executed: {command}");
+        }
 
-        return cmd.ExitCode;
+        cmd.Close();
+        return ExitCode;
     }
 
     private static void RevertChanges() {
